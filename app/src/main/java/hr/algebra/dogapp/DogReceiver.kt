69a9b0d8656960953e598c6fe838c9eccc2823c2
Ante Prskalo <aprskalo@racunarstvo.hr>
2024-01-10 00:46:03 +0100

    Minor changes.

--- app/src/main/java/hr/algebra/dogapp/DogReceiver.kt
+++ app/src/main/java/hr/algebra/dogapp/DogReceiver.kt
@@ -3,18 +3,22 @@ package hr.algebra.dogapp
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import hr.algebra.dogapp.framework.callDelayed
 import hr.algebra.dogapp.framework.startActivity
 
 class DogReceiver : BroadcastReceiver() {
-    val apiStatusActive: Boolean
+    private val apiStatusActive: Boolean
         get() {
             return Intent().getBooleanExtra("apiStatusActive", false)
         }
 
     override fun onReceive(context: Context, intent: Intent) {
         if (apiStatusActive) {
-            if (context is SplashScreenActivity) context.finish()
+            if (context is SplashScreenActivity) {
+                callDelayed(3000L) {
+                    context.finish()
+                }
+            }
         } else {
             context.startActivity<HostActivity>()
         }

9f02f279187b02b3a96ff4e4fde37814a17376fd
Ante Prskalo <aprskalo@racunarstvo.hr>
2024-01-10 17:11:35 +0100

    Minor changes.

--- app/src/main/java/hr/algebra/dogapp/fragment/FavouritesFragment.kt
+++ app/src/main/java/hr/algebra/dogapp/fragment/FavouritesFragment.kt
@@ -24,7 +24,6 @@ class FavouritesFragment : Fragment() {
     ): View? {
         binding = FragmentFavouritesBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
